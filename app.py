33a008b732993b0c30d94f2f914ed647e73eb3af
Francis King Uyguangco <20160009778@my.xu.edu.ph>
2024-12-03 00:02:53 +0800

    recipe/name/ingredients function

--- app.py
+++ app.py
@@ -9,15 +9,74 @@ recipes_collection = db['Recipes']
 ingredients_collection = db['Ingredients']
 dietarybenefits_collection = db['Dietarybenefits']
 nutritioninfo_collection = db['Nutritioninfo']
-pictures_collection = db['Pictures']
 users_collection = db['Users']
-videos_collection = db['Videos']
+
 
 @app.route('/', methods=['GET'])
 def getAll():
-    recipes = list(recipes_collection.find({},{'_id': 0}))
-    return jsonify(recipes)
+    recipes = recipes_collection.find({})
+    response = []
+    for recipe in recipes:
+        recipe_ingredients = ingredients_collection.find({'recipeID': recipe['recipeID']})
+        ingredients = [{'name': ing['name'], 'quantity': ing['quantity']} for ing in recipe_ingredients]
 
+        recipe_dietarybenefits = dietarybenefits_collection.find({'recipeID': recipe['recipeID']})
+        dietarybenefits = [
+            {
+                'is_vegan': diet['is_vegan'],
+                'is_vegetarian': diet['is_vegetarian'],
+                'is_gluten_free': diet['is_gluten_free'],
+                'allergens': diet['allergens']
+            } 
+            for diet in recipe_dietarybenefits
+        ]
+        response.append({
+            'recipeID': recipe['recipeID'],
+            'name': recipe['name'],
+            'description': recipe.get('description', ''),
+            'origin': recipe.get('origin', ''),
+            'category': recipe.get('type', ''),
+            'serving': recipe.get('servings', ''),
+            'preptime': recipe.get('prep_time', ''),
+            'cooktime': recipe.get('cook_time', ''),
+            'difficulty': recipe.get('difficulty', ''),
+            'majorIngredient': recipe.get('majorIngredient', ''),
+            'instructions': recipe.get('instructions', ''),
+            'createdDate': recipe.get('createdAt', ''),
+            'UpdatedDate': recipe.get('updatedAt', ''),
+            'ingredients': ingredients,
+            'dietarybenefits': dietarybenefits
+        })
+    
+    return jsonify(response)
+
+
+@app.route('/recipes/<name>/ingredients', methods=['GET'])
+def getIngredients(name):
+    try:
+        recipe = recipes_collection.find_one({'name': {'$regex': f'^{name}$', '$options': 'i'}}, {'_id': 0})
+        
+        if not recipe:
+            return jsonify({"message": f"Recipe '{name}' not found"}), 404
+        
+        recipe_id = recipe['recipeID']
+        ingredients_cursor = ingredients_collection.find({'recipeID': recipe_id})
+        
+        ingredients = [{'name': ing['name'], 'quantity': ing['quantity']} for ing in ingredients_cursor]
+        
+        response = {
+            'recipeID': recipe['recipeID'],
+            'name': recipe['name'],
+            'description': recipe.get('description', ''),
+            'majorIngredient': recipe.get('majorIngredient', ''),
+            'ingredients': ingredients
+        }
+        
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
-
